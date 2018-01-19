fde711eca1b3a91c09d8a793ff40a736780db953
Sugandha <raosugandha12@gmail.com>
2018-01-19 17:30:59 +0000

    made changes to styles

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,20 +1,18 @@
 var gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-mixins = require('postcss-mixins'),	
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	mixins = require('postcss-mixins')
+	nested = require('postcss-nested'),
+	cssImport = require('postcss-import');
+
+
+
 
 
 gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, mixins,  cssvars, nested, autoprefixer]))
-	.on('error', function(errorInfo){
-		console.log(errorInfo.toString());
-		this.emit('end');
-	})
+return	gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssImport, mixins, nested, autoprefixer]))
 	
-	
-.pipe(gulp.dest('./app/temp/styles'));
+	.pipe(gulp.dest('./app/temp/styles'));
+
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
+    watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
 
-browserSync = require('browser-sync').create();		
 
 
 
@@ -12,27 +12,24 @@ gulp.task('watch', function(){
 			baseDir: "app"
 		}
 	});
-	
-	
 	watch('./app/index.html', function(){
 		browserSync.reload();
 	});
 	
 	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('cssInject');
+		gulp.start('CSSInject');
 	});
 	
 	watch('./app/assets/scripts/**/*.js', function(){
 		gulp.start('scriptsRefresh');
 	})
-	
 });
 
 
-gulp.task('cssInject', ['styles'], function(){
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
+gulp.task('CSSInject', ['styles'], function(){
+	return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
 });
+
 
 gulp.task('scriptsRefresh', ['scripts'], function(){
 	browserSync.reload();
